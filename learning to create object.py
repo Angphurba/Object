85f95d1b8c84288d35cc22bc47462aeaedaa8a90
Angphurba Sherpa <44555127+Angphurba@users.noreply.github.com>
2018-12-23 22:06:51 -0500

    Add files via upload

---
+++ learning to create object.py
@@ -0,0 +1,26 @@
+class Clothing():
+        clothing_a = 2
+        clothing_b = 3
+        clothing_c = 4
+        clothing_d = 3
+        clothing_e = 2
+     
+        def my_clothing(self):
+         self.my_clothing_a = clothing_a
+         self.my_clothing_b = clothing_b
+         self.my_clothing_c = clothing_c
+         self.my_clothing_d = clothing_d
+         self.my_clothing_e = clothing_e
+
+clothing_cap = Clothing()
+clothing_jacket = Clothing()
+clothing_tshirt = Clothing()
+clothing_pant = Clothing()
+clothing_shoe = Clothing()
+    
+
+print 'I have',clothing_cap.clothing_a, 'pairs of cap.'
+print 'I have',clothing_jacket.clothing_b, 'pairs of jacket.'
+print 'I have',clothing_tshirt.clothing_c, 'pairs of tshirt.'
+print 'I have',clothing_pant.clothing_d, 'pairs of pant.'
+print 'I have',clothing_shoe.clothing_e, 'pairs of shoe.'
